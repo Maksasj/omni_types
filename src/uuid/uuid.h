e4af7cd0e72d4b5db23a81d41832fcdf02c0560d
maksasj <radioboos@gmail.com>
2023-10-25 17:37:50 +0300

    uuid class splitted

--- src/uuid/uuid.h
+++ src/uuid/uuid.h
@@ -25,46 +25,17 @@ namespace omni::types {
         private:
             u64 _data[2];
         
-            UUID(const u64& data1, const u64& data2) {
-                _data[0] = data1;
-                _data[1] = data2;
-            }
+            UUID(const u64& data1, const u64& data2);
 
         public:
-            UUID() {
-                *this = UUID(0, 0);
-            }
+            UUID();
             
-            bool operator==(const UUID& other) const {
-                return this->_data[0] == other._data[0] && this->_data[1] == other._data[1];
-            }
+            bool operator==(const UUID& other) const;
+            bool operator!=(const UUID& other) const;
 
-            bool operator!=(const UUID& other) const {
-                return !(*this == other);
-            }
+            std::string to_string() const;
 
-            std::string to_string() const {
-                std::stringstream ss;
-                ss << std::hex << std::setfill('0') << std::setw(16) << _data[0];
-                ss << std::hex << std::setfill('0') << std::setw(16) << _data[1];
-
-                std::string tmp = ss.str();
-                tmp.insert(8, "-");
-                tmp.insert(13, "-");
-                tmp.insert(18, "-");
-                tmp.insert(23, "-");
-
-                return tmp;
-            }
-
-            static UUID gen() {
-                static std::random_device rd;
-                static std::mt19937_64 gen(rd());
-
-                static std::uniform_int_distribution<u64> dist(0ULL, std::numeric_limits<u64>::max());
-
-                return UUID(dist(gen), dist(gen));
-            }
+            static UUID gen();
     };
 }
 
